c0a3d32a4790e889ab9ff5f8c56baeb41a227c00
Arthur Pariente <arthur.pariente78960@gmail.com>
2025-10-15 18:59:57 +0200

    fix: docker entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,11 @@ mesg n 2>/dev/null || true
 
 cd /root
 
-if [ -f ./is-workspace/install/setup.bash ]; then
+if [ -f /is-workspace/install/setup.bash ]; then
   # shellcheck disable=SC1091
-  . ./is-workspace/install/setup.bash
+  . /is-workspace/install/setup.bash
 else
-  echo "./is-workspace/install/setup.bash not found"
+  echo "/is-workspace/install/setup.bash not found"
 fi
 
 integration-service ./dds_to_ws.yaml
